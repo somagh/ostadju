ea70b3ec8236f4dba45bb876644480111ced002c
somagh <aryakowsary@gmail.com>
2018-10-04 11:57:52 +0330

    ids fixed.

--- ostadju/urls.py
+++ ostadju/urls.py
@@ -19,5 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',TemplateView.as_view(template_name='home.html'))
+    url(r'^$',TemplateView.as_view(template_name='home.html'),name="home")
 ]
