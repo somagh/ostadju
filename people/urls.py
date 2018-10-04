3626748f1583975a68f69685122149f10676b83e
somagh <aryakowsary@gmail.com>
2018-10-04 15:18:10 +0330

    feature 3

--- people/urls.py
+++ people/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 from people import views
 
 
 urlpatterns = [
-    url(r'^signup/$', views.signup, name="signup")
+    url(r'^signup/$', views.signup, name="signup"),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='people/login.html'), name='login'),
 ]
