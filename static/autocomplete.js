c94fef1f83f8dcf3fd36b7f3fb3b2b802807e7ba
somagh <aryakowsary@yahoo.com>
2018-11-20 12:47:10 +0330

    autocomplete done

---
+++ static/autocomplete.js
@@ -0,0 +1,11 @@
+document.getElementById('search_profiles_form').addEventListener('input', function (e) {
+    results = document.getElementById('autocomplete_results');
+    fetch("http://localhost:8000/search_teachers_api?query=" + e.target.value).then(function (response) {
+        return response.json()
+    }).then(function (json) {
+        results.innerHTML='';
+        json.map(function (teacher) {
+            results.innerHTML += '<a href="http://localhost:8000'+teacher.profile_url+'">' + teacher.first_name + ' ' + teacher.last_name + '</a>'
+        })
+    })
+});
